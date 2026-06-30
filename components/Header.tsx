02460a92ef3743afa5623740138b7e068373d489
GalaxyMatrix <coranchunilall47@gmail.com>
2026-06-30 22:22:47 +0200

    Implemented NAV BAR and dropdown

---
+++ components/Header.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link"
+import Image from "next/image"
+import Navbar from "./Navbar"
+import Userdropdown from "./Userdropdown"
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 header">
+        <div className="container header-wrapper">
+            <Link href="/">
+                <Image src="/assets/icons/logo.svg" alt="Signalist" width={140} height={32} className="h-8 w-auto cursor-pointer" />
+            </Link>
+            <nav className="hidden sm:block">
+              <Navbar />
+
+            </nav>
+          <Userdropdown />
+        </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const NAV_LINKS = [ 
+    {href: '/', label: 'Dashboard'},
+    {href: '/search', label: 'Search'},
+    {href: '/watchlist', label: 'Watchlist'},
+    
+]
