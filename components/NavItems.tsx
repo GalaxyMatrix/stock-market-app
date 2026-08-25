3c6cb4bbf12db6c9891a4143a799e89420739dac
GalaxyMatrix <coranchunilall47@gmail.com>
2026-08-25 23:11:01 +0200

    Implemented home page

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { NAV_LINKS } from '@/lib/constants'
+import { NAV_ITEMS } from '@/lib/constants'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
@@ -17,7 +17,7 @@ const NavItems = () => {
 
   return (
     <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium ">
-      {NAV_LINKS.map(({href, label}) => 
+      {NAV_ITEMS.map(({href, label}) => 
       <li key={href}>
         <Link href={href} className={`hover:text-yellow-500 transition-colors ${
                         isActive(href) ? 'text-gray-100' : ''
