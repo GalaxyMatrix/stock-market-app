199f1415c4cecd70640958790c107d9c8e0abfdc
GalaxyMatrix <coranchunilall47@gmail.com>
2026-09-05 00:36:56 +0200

    Implemented Database

---
+++ database/mongoose.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const MONGO_URI = process.env.MONGODB_URI;
+
+declare global {
+    var mongooseCache: {
+        conn: typeof mongoose | null;
+        promise: Promise<typeof mongoose> | null;
+
+    }
+}
+
+let cached = global.mongooseCache;
+
+if (!cached) {
+    cached = global.mongooseCache = { conn: null, promise: null };
+}
+
+export const connectToDatabase = async () => {
+    if(!MONGO_URI) {
+        throw new Error('MONGODB_URI is not defined');
+    }
+
+    if(cached.conn) {
+        return cached.conn;
+    }
+
+    if(!cached.promise) {
+        cached.promise = mongoose.connect(MONGO_URI).then((mongoose) => {
+            return mongoose;
+        });
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (err) {
+        cached.promise = null;
+        throw err;
+    }
+
+    console.log(`Connected to database ${process.env.NODE_ENV} - ${MONGO_URI}`);
+
+    return cached.conn;
+}
